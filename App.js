c8df5866a3b912441602651feaa36fad821dfc23
Olsi <olsi.hoxha.08@protonmail.ch>
2020-10-19 23:45:22 +0200

    created the project

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import * as React from 'react';
+import { Text, StatusBar, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import Donut from './Donut'
+
+const data = [{
+  percentage: 8,
+  color: 'tomato',
+  max: 10
+}, {
+  percentage: 14,
+  color: 'skyblue',
+  max: 20
+}, {
+  percentage: 92,
+  color: 'gold',
+  max: 100
+}, {
+  percentage: 240,
+  color: '#222',
+  max: 500
+}]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden/>
+      <View style={{flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap', alignItems: 'center'}}>
+        {data.map((p, i) => {
+          return <Donut key={i} percentage={p.percentage} color={p.color} delay={500 + 100 * i} max={p.max}/>
+        })}
+      </View>
     </View>
   );
 }
@@ -14,8 +38,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#fff',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
